079ebe1fa70be1b32a92edc77977cc9045ef0b42
youcai <omegacoleman@gmail.com>
2026-01-29 14:44:41 +0000

    add microbenchmarks

--- tokenizers/benches/llama3_peek2_benchmark.rs
+++ tokenizers/benches/llama3_peek2_benchmark.rs
@@ -8,10 +8,43 @@ use criterion::{Criterion, Throughput};
 use std::hint::black_box;
 use tokenizers::{
     models::{bpe::BpeTrainerBuilder, TrainerWrapper},
-    EncodeInput, Tokenizer,
+    pre_tokenizers::peektwo::PeekTwo,
+    pre_tokenizers::split::Split,
+    EncodeInput, Tokenizer, PreTokenizer, PreTokenizedString,
 };
 
 static BATCH_SIZE: usize = 1_000;
+
+fn run_pretokenizer_benches(c: &mut Criterion, prefix: &str, pretok: &impl PreTokenizer) {
+    let data = std::fs::read_to_string("data/big.txt").unwrap();
+    let mut group = c.benchmark_group(format!("pretok-{}", prefix));
+    group.throughput(Throughput::Bytes(data.len() as u64));
+    group.bench_function("pretok-big", |b| {
+        let mut pretokenized = PreTokenizedString::from(black_box(data.clone()));
+        b.iter(|| {
+            pretok.pre_tokenize(&mut pretokenized).unwrap()
+        })
+    });
+}
+
+pub fn pretok_original(c: &mut Criterion) {
+    let split_s = r#"
+    {
+        "type": "Split",
+        "pattern": {
+          "Regex": "(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\\r\\n\\p{L}\\p{N}]?\\p{L}+|\\p{N}{1,3}| ?[^\\s\\p{L}\\p{N}]+[\\r\\n]*|\\s*[\\r\\n]+|\\s+(?!\\S)|\\s+"
+        },
+        "behavior": "Isolated",
+        "invert": false
+    }"#;
+    let pretok = serde_json::from_str::<Split>(split_s).unwrap();
+    run_pretokenizer_benches(c, "original", &pretok);
+}
+
+pub fn pretok_peek2(c: &mut Criterion) {
+    let pretok = PeekTwo::new().unwrap();
+    run_pretokenizer_benches(c, "peek2", &pretok);
+}
 
 fn run_llama3_benches(c: &mut Criterion, prefix: &str, tokenizer_path: &str) {
     let data = std::fs::read_to_string("data/big.txt").unwrap();
@@ -72,7 +105,7 @@ pub fn llama3_peek2(c: &mut Criterion) {
 criterion_group! {
     name = llama_3;
     config = Criterion::default().sample_size(10);
-    targets = llama3_original, llama3_peek2
+    targets = llama3_original, llama3_peek2, pretok_original, pretok_peek2
 }
 
 criterion_main!(llama_3);
